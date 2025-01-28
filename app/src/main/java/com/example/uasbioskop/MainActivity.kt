ceba9aabe0f54825daffaf51f4cf06ba7d8af02b
buyaramadhan <ytchochote@gmail.com>
2025-01-28 21:03:24 +0700

    menambah kelas MainActivity untuk mengelola aktivitas utama aplikasi

---
+++ app/src/main/java/com/example/uasbioskop/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.uasbioskop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.uasbioskop.ui.ViewModel.FilmApp
+import com.example.uasbioskop.ui.theme.UASBioskopTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UASBioskopTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    FilmApp(
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UASBioskopTheme {
+        Greeting("Android")
+    }
+}
