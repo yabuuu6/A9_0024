3d1cb1d194fbde5ec8a23a8725709d38bae7a37f
buyaramadhan <ytchochote@gmail.com>
2025-01-28 20:13:12 +0700

    membuat data class tiket

---
+++ app/src/main/java/com/example/uasbioskop/model/Tiket.kt
@@ -0,0 +1,35 @@
+package com.example.uasbioskop.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Tiket(
+    @SerialName("id_tiket")
+    val idTiket: Int,
+    @SerialName("id_penayangan")
+    val idPenayangan: Int,
+    @SerialName("jumlah_tiket")
+    val jumlahTiket: Int,
+    @SerialName("total_harga")
+    val totalHarga: Int,
+    @SerialName("status_pembayaran")
+    val statusPembayaran: String,
+    @SerialName("harga_tiket")
+    val hargaTiket: Int? = null,
+)
+
+
+@Serializable
+data class AllTiketResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Tiket>
+)
+
+@Serializable
+data class TiketDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Tiket
+)
